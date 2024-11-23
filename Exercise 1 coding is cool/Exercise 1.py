9922a5876bcba008036207ffabae340c8984f62d
hrishaabh07 <hrishaabh07@gmail.com>
2024-11-23 13:30:00 +0400

    Exercise 1.py

---
+++ Exercise 1 coding is cool/Exercise 1.py
@@ -0,0 +1,10 @@
+# Declare and initialize variables for the components of the sentence
+word_one = "Coding"
+word_two = "is"
+word_three = "Cool"
+
+# Combine the variables using string concatenation to create a sentence
+sentence = word_one + " " + word_two + " " + word_three
+
+# Output the constructed sentence to the console
+print(sentence)
